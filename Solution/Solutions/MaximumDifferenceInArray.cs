3761226bec59b0111657fc4a1409d1707c1966e2
xialinbo <xialinbo@hotmail.com>
2018-04-06 23:44:08 +0800

    change to use a simpler and faster way

--- Solution/Solutions/MaximumDifferenceInArray.cs
+++ Solution/Solutions/MaximumDifferenceInArray.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.Linq;
 
 namespace Solution.Solutions
@@ -7,58 +8,31 @@ namespace Solution.Solutions
     {
         public int GetMaxDifference(int[] numbers)
         {
-            var numberDistibution = new SortedDictionary<int, List<int>>();
-            for(int i = 0; i < numbers.Length; i++)
-            {
-                var number = numbers[i];
-                if (!numberDistibution.ContainsKey(number))
-                {
-                    numberDistibution[number] = new List<int>();
-                }
+            var count = numbers.Length;
+            var minNumberArray = new int[count];
+            var maxNumberArray = new int[count];
 
-                numberDistibution[number].Add(i);
+            minNumberArray[0] = numbers[0];
+            for (int i = 1; i < count; i++)
+            {
+                minNumberArray[i] = Math.Min(numbers[i], minNumberArray[i - 1]);
             }
 
-            var minPointer = 0;
-            var maxPointer = numberDistibution.Count - 1;
-
-            var minPointerUpperLimit = minPointer;
-            var maxPointerLowerLimit = maxPointer;
-
-            while (!IsAValidAnswer(numberDistibution, minPointerUpperLimit, maxPointer) && minPointerUpperLimit < maxPointer)
+            maxNumberArray[count - 1] = numbers[count - 1];
+            for (int i = count - 2; i >= 0; i--)
             {
-                minPointerUpperLimit++;
+                maxNumberArray[i] = Math.Max(numbers[i], maxNumberArray[i + 1]);
             }
 
-            while (!IsAValidAnswer(numberDistibution, minPointer, maxPointerLowerLimit) && maxPointerLowerLimit > minPointer)
+            var differences = new List<int>();
+            for (int i = 0; i < count; i++)
             {
-                maxPointerLowerLimit--;
+                differences.Add(maxNumberArray[i]-minNumberArray[i]);
             }
 
-            var result = new List<int>();
-            for (int i = minPointer; i <= minPointerUpperLimit; i++)
-            {
-                for (int j = maxPointer; j >= maxPointerLowerLimit; j--)
-                {
-                    var diff = numberDistibution.ElementAt(j).Key -
-                               numberDistibution.ElementAt(i).Key;
+            var result = differences.Max();
 
-                    if (IsAValidAnswer(numberDistibution, i, j))
-                    {
-                        result.Add(diff);
-                    }
-                }
-            }
-
-            return result.Max() > 0 ? result.Max() : -1;
-        }
-
-        private bool IsAValidAnswer(SortedDictionary<int, List<int>> numberDistibution, int minPointer, int maxPointer)
-        {
-            var indexListMin = numberDistibution.ElementAt(minPointer).Value;
-            var indexListMax = numberDistibution.ElementAt(maxPointer).Value;
-
-            return indexListMin.Min() < indexListMax.Max();
+            return result > 0 ? result : -1;
         }
     }
 }
